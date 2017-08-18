227d81c36082d94cfa7cb406899128a61cf3f99b
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-08-17 22:36:11 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -2758,6 +2758,7 @@ CREATE VIEW v_solicitud_informes AS
         END AS a_tiempo
    FROM (cor1440_gen_proyectofinanciero p
      JOIN v_solicitud_informes1 s ON ((p.id = s.proyectofinanciero_id)))
+  WHERE (p.id = ANY (ARRAY[101, 111]))
   ORDER BY s.fechaplaneada;
 
 
