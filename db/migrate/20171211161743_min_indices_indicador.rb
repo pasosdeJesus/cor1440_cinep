51c425e450bce736259c8974c2cc1e2040a20126
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-12-11 18:50:13 -0500

    mejora indice

--- db/migrate/20171211161743_min_indices_indicador.rb
+++ db/migrate/20171211161743_min_indices_indicador.rb
@@ -6,7 +6,7 @@ class MinIndicesIndicador < ActiveRecord::Migration[5.1]
       SELECT pg_catalog.setval('cor1440_gen_tipoindicador_id_seq', 101, false);
       SELECT pg_catalog.setval('cor1440_gen_resultadopf_id_seq', 101, false);
       SELECT pg_catalog.setval('cor1440_gen_objetivopf_id_seq', 101, false);
-      SELECT pg_catalog.setval('cor1440_gen_proyectofinanciero_id_seq', GREATEST(MAX(id), 101), false) FROM cor1440_gen_proyectofinanciero;
+      SELECT pg_catalog.setval('cor1440_gen_proyectofinanciero_id_seq', 1+GREATEST(MAX(id), 100), false) FROM cor1440_gen_proyectofinanciero;
     SQL
   end
   def down 
