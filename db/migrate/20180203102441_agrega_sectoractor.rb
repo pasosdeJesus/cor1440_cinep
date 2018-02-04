7127fdf24b464b866673589284a35b248618f1ca
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-02-03 21:38:58 -0500

    mejora migracion

--- db/migrate/20180203102441_agrega_sectoractor.rb
+++ db/migrate/20180203102441_agrega_sectoractor.rb
@@ -1,10 +1,10 @@
 class AgregaSectoractor < ActiveRecord::Migration[5.1]
   def up
     execute <<-SQL
-      INSERT INTO sectoractor (id, nombre, fechacreacion, fechadeshabilitacion,
-        created_at, updated_at) 
+      INSERT INTO sectoractor (id, nombre, fechacreacion, created_at, 
+        updated_at) 
         VALUES (18, 'FORMULA POLÍTICAS PÚBLICAS', '2018-02-03', '2018-02-03',
-        '2018-02-03', '2018-02-03');
+        '2018-02-03');
     SQL
   end
   def down
