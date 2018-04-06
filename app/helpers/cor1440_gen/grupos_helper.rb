fcacf2279fa0c4e294fa7684ec6416d5a0c134f3
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-04-06 17:05:34 -0500

    Directorio es organigrama tambien, hay grupos especiales por ahora Desactivados, Usuarios y Externos

--- app/helpers/cor1440_gen/grupos_helper.rb
+++ app/helpers/cor1440_gen/grupos_helper.rb
@@ -6,9 +6,9 @@ module Cor1440Gen
     def self.mis_grupos_sinus(current_usuario)
       if current_usuario.rol == Ability::ROLADMIN || 
            current_usuario.rol == Ability::ROLDIR
-        misgrupossinu = Sip::Grupo.habilitados.where("nombre<>'usuario'")
+        misgrupossinu = Sip::Grupo.habilitados.where("nombre<>'Usuarios'")
       else
-        cg = ::ApplicationHelper.supergrupos_usuario(current_usuario) - ['usuario']
+        cg = ::ApplicationHelper.supergrupos_usuario(current_usuario) - ['Usuarios']
         misgrupossinu = Sip::Grupo.habilitados.where("nombre IN ('#{cg.join("', '")}')")
       end
     end
