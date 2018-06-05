6c27c033a83cc20c0d11a1fa38dc4ec8f74604fb
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-06-05 17:30:48 -0500

    valida que grupos formen arbol

--- app/models/sip/grupo.rb
+++ app/models/sip/grupo.rb
@@ -34,5 +34,37 @@ module Sip
       validate: true
     has_many :subgrupo, through: :grupo_subgrupo,
       class_name: 'Sip::Grupo'
+
+    validate :subgrupos_son_arbol
+    def subgrupos_son_arbol
+      gp = rec_subgrupo_son_arbol(self, [id])
+      if gp != []
+        lc = gp.map { |lg| lg.join(" -> ") }
+        errors.add(:subgrupo, "Problema(s) en rama(s): #{lc.join('; ')}")
+      end
+    end
+
+    # Decide que las identificaciones de grupos en visitados no 
+    # están en el subárbol que comienza en grupo.
+    # Retorna lista de ciclos que encuentra o vació si no hay
+    def rec_subgrupo_son_arbol(grupo, visitados)
+      sub = grupo.subgrupo
+      if sub.count > 0
+        sids = sub.map(&:id)
+        int = sids & visitados
+        if (int != [])
+          inom = int.map {|i| [Sip::Grupo.find(i).nombre]}
+          return inom
+        end
+        sub.each do |g|
+          gp = rec_subgrupo_son_arbol(g, visitados + sids)
+          if  gp != []
+            return gp.map { |sg| [grupo.nombre] + sg }
+          end
+        end
+      end
+      return []
+    end
+
   end
 end
