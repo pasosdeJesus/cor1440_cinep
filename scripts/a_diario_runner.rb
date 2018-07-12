c91982ccc8a1eff58d8a946e082f590703abd9c0
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-07-11 20:52:51 -0500

    En correo de alerta, referencia cinep en rojo y enviada con 20 dias de anticipacion

--- scripts/a_diario_runner.rb
+++ scripts/a_diario_runner.rb
@@ -19,52 +19,52 @@ def run
   puts "Inicio de verificacion alertas proyectofinanciero"
 
   hoy = Date.today
-  hoymassiete = hoy + 7
-  puts "Verifica cierre"
+  hoymasveinte = hoy + 20
+  puts "Verifica cierre el #{hoymasveinte}"
   Cor1440Gen::Proyectofinanciero.
-    where(fechacierre: hoymassiete).each do |p|
-    envia(p.id, 'cierra', 'en una semana', p.fechacierre_localizada)
+    where(fechacierre: hoymasveinte).each do |p|
+    envia(p.id, 'cierra', 'en 20 días', p.fechacierre_localizada)
   end
   puts "Verifica liquidacion"
   Cor1440Gen::Proyectofinanciero.
-    where(fechaliquidacion: hoymassiete).each do |p|
-    envia(p.id, 'se liquida', 'en una semana', p.fechacierre_localizada)
+    where(fechaliquidacion: hoymasveinte).each do |p|
+    envia(p.id, 'se liquida', 'en 20 días', p.fechaliquidacion_localizada)
   end
   puts "Verifica desembolsos"
   ::Desembolso.
-    where(fechaplaneada: hoymassiete).each do |d|
+    where(fechaplaneada: hoymasveinte).each do |d|
     envia(d.proyectofinanciero_id, 'tiene un desembolso', 
-          'en una semana', d.fechaplaneada_localizada, 
+          'en 20 días', d.fechaplaneada_localizada, 
           [ "Concepto: #{d.detalle}",
             "Valor planeado: #{d.valorplaneado_localizado}"])
   end
   puts "Verifica informes narrativos"
   ::Informenarrativo.
-    where(fechaplaneada: hoymassiete).each do |d|
+    where(fechaplaneada: hoymasveinte).each do |d|
     envia(d.proyectofinanciero_id, 'tiene un informe narrativo', 
-          'en una semana', d.fechaplaneada_localizada,
+          'en 20 días', d.fechaplaneada_localizada,
           [ "Detalle: #{d.detalle}"])
   end
   puts "Verifica informes financiero"
   ::Informefinanciero.
-    where(fechaplaneada: hoymassiete).each do |d|
+    where(fechaplaneada: hoymasveinte).each do |d|
     envia(d.proyectofinanciero_id, 'tiene un informe financiero', 
-          'en una semana', d.fechaplaneada_localizada,
+          'en 20 días', d.fechaplaneada_localizada,
           [ "Detalle: #{d.detalle}"])
   end
   puts "Verifica evaluciones"
   ::Informeevaluacion.
-    where(fechaplaneada: hoymassiete).each do |d|
-    puts "El proyecto #{d.proyectofinanciero_id} tiene una evaluación planeada en una semana, el #{d.fechaplaneada}"
+    where(fechaplaneada: hoymasveinte).each do |d|
+    puts "El proyecto #{d.proyectofinanciero_id} tiene una evaluación planeada en 20 días, el #{d.fechaplaneada}"
     envia(d.proyectofinanciero_id, 'tiene una evaluación', 
-          'en una semana', d.fechaplaneada_localizada,
+          'en 20 días', d.fechaplaneada_localizada,
           [ "Detalle: #{d.detalle}"])
   end
   puts "Verifica auditorias"
   ::Informeauditoria.
-    where(fechaplaneada: hoymassiete).each do |d|
+    where(fechaplaneada: hoymasveinte).each do |d|
     envia(d.proyectofinanciero_id, 'tiene una auditoria', 
-          'en una semana', d.fechaplaneada_localizada,
+          'en 20 días', d.fechaplaneada_localizada,
           [ "Detalle: #{d.detalle}",
             "Presupuesto donante: #{d.presupuestodonante}",
             "Presupuesto CINEP/PPP: #{d.presupuestodonante}",
@@ -72,9 +72,9 @@ def run
   end
   puts "Verifica productos"
   ::Productopf.
-    where(fechaplaneada: hoymassiete).each do |d|
+    where(fechaplaneada: hoymasveinte).each do |d|
     envia(d.proyectofinanciero_id, 'tiene un producto', 
-          'en una semana', d.fechaplaneada_localizada,
+          'en 20 días', d.fechaplaneada_localizada,
           [ "Tipo: #{d.tipoproductopf.nombre if d.tipoproductopf_id}",
             "Detalle: #{d.detalle}"
           ])
