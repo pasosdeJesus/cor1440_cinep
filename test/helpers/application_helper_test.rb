90a3c7dc5f97394745383b70e9224a778e913289
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-09-02 20:03:01 -0500

    Actualiza

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -6,7 +6,7 @@ class ApplicationHelperTest < ActionView::TestCase
   include ApplicationHelper
 
   test "dif_meses_dias" do
-    d = dif_meses_dias(Date.new(2016, 03, 01), Date.new(2016, 06, 30)) 
+    d = ApplicationHelper.dif_meses_dias(Date.new(2016, 03, 01), Date.new(2016, 06, 30)) 
     puts d
     assert d == '4 meses'
   end
