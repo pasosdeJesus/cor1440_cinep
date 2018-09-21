9eac260b919d79c7f4149298a4728a6f8a01d497
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-09-21 07:45:06 -0500

    Avanzan formatos GH desde resumen de usuario

--- db/migrate/20180920124019_ajusta_rep_usuario.rb
+++ db/migrate/20180920124019_ajusta_rep_usuario.rb
@@ -9,6 +9,11 @@ class AjustaRepUsuario < ActiveRecord::Migration[5.2]
         SET ruta='Gestion Humana/reporte_usuario.ods' WHERE id=10;
       UPDATE heb412_gen_plantilladoc
         SET ruta='Gestion Humana/reporte_usuario.odt' WHERE id=10;
+
+      UPDATE heb412_gen_campoplantillahcr SET nombrecampo='presenta_nombre'
+        WHERE plantillahcr_id=10 AND id='322';
+      DELETE FROM heb412_gen_campoplantillahcr 
+        WHERE plantillahcr_id=10 AND id='323';
     SQL
   end
 end
