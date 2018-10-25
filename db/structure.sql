cb1a8f05e301503c38426219a452d13aafd141bf
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-10-25 07:05:22 -0500

    actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -7356,7 +7356,7 @@ ALTER TABLE ONLY public.sip_ubicacion
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO "$user", public;
+SET search_path TO public, pg_catalog;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20131128151014'),
