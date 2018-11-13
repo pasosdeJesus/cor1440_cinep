55d3c8ecee801dc0c66d6fabae0d89c17bf86a6d
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-11-13 06:32:06 -0500

    renumera actividadtipo

--- db/migrate/20181113094541_e1i1_como_resultado.rb
+++ db/migrate/20181113094541_e1i1_como_resultado.rb
@@ -8,16 +8,16 @@ class E1i1ComoResultado < ActiveRecord::Migration[5.2]
           '2018-11-13','2018-11-13','2018-11-13');
       INSERT INTO cor1440_gen_campoact (id, actividadtipo_id,
         nombrecampo, ayudauso, tipo) 
-        VALUES (5, 83, '¿Es propiciado por CINEP/PPP?', '', 4);
+        VALUES (16, 83, '¿Es propiciado por CINEP/PPP?', '', 4);
       INSERT INTO cor1440_gen_campoact (id, actividadtipo_id,
         nombrecampo, ayudauso, tipo) 
-        VALUES (6, 83, '¿Se creó para tratar un conflicto?', '', 4);
+        VALUES (17, 83, '¿Se creó para tratar un conflicto?', '', 4);
       INSERT INTO cor1440_gen_campoact (id, actividadtipo_id,
         nombrecampo, ayudauso, tipo) 
-        VALUES (7, 83, '¿Participa más de un sector?', '', 4);
+        VALUES (18, 83, '¿Participa más de un sector?', '', 4);
       INSERT INTO cor1440_gen_campoact (id, actividadtipo_id,
         nombrecampo, ayudauso, tipo) 
-        VALUES (8, 83, '¿Se mantiene por lo menos dos veces?', '', 4);
+        VALUES (19, 83, '¿Se mantiene por lo menos dos veces?', '', 4);
       UPDATE cor1440_gen_actividadpf SET actividadtipo_id='83' WHERE id='38';
     SQL
   end
