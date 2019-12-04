05e938681ec31ddae84864d2a9b9c7ef40452ef5
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-12-04 14:45:14 -0500

    Actualiza

--- app/controllers/cor1440_gen/proyectosfinancieros_controller.rb
+++ app/controllers/cor1440_gen/proyectosfinancieros_controller.rb
@@ -12,8 +12,12 @@ module Cor1440Gen
 
     before_action :set_proyectofinanciero, 
       only: [:show, :edit, :update, :destroy]
-    load_and_authorize_resource class: ::Cor1440Gen::Proyectofinanciero,
-      except: [:duracion]
+
+    load_and_authorize_resource  class: Cor1440Gen::Proyectofinanciero,
+      only: [:new, :create, :destroy, :edit, :update, :index, :show,
+             :objetivospf]
+    #load_and_authorize_resource class: ::Cor1440Gen::Proyectofinanciero,
+    #  except: [:duracion]
 
     include ::Sip::ConsultasHelper
 
