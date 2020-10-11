e71731f99642ef4ad1fe6ff5a8de949c6e0961bd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-10-11 13:54:26 -0500

    precompila

--- bin/precompila.sh
+++ bin/precompila.sh
@@ -6,8 +6,9 @@ fi;
 
 
 if [ "$RAILS_ENV" = "" ] ; then
-  RAILS_ENV=developtment
+  RAILS_ENV=development
 fi
-
+rm -rf public/assets/*
+rm -rf public/packs/*
 RAILS_ENV=$RAILS_ENV PGSSLCERT=$PGSSLCERT PGSSLKEY=$PGSSLKEY bin/rails \
   assets:precompile --trace
