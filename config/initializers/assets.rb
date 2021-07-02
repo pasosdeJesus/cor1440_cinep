be97fc54dac7110db4f9419791bb698cf244fd20
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-07-01 22:06:48 -0500

    Usa mapa_tiempo de forma mas robusta

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,11 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+
+Rails.application.config.assets.paths << Rails.root.join('node_modules/@pasosdejesus/mapa_tiempo_yi_liu/')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( dist/index.bundle.js dist/index.bundle.js.map )
